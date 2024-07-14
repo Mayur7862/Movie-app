af128f4b2bc8642440db931ed3f4562931c870a5
Mayur7862 <83858995+Mayur7862@users.noreply.github.com>
2024-07-15 00:41:57 +0530

    use effect error

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -2,14 +2,16 @@
 import React from 'react'
 import { useEffect } from 'react'
 
-export default function error({error, reset}) {
+export default function Error({ error, reset }) {
     useEffect(() => {
         console.log(error);
-    },[error])
-  return (
-    <div className='text-center mt-10'>
-      <h1>Kahi to chud gaya pura app Firse try karle </h1>
-      <button className='hover:text-amber-600 ' onClick={() => reset()}> Fir se karle </button>
-    </div>
-  )
+    }, [error]);
+    
+    return (
+        <div className='text-center mt-10'>
+            <h1>Kahi to chud gaya pura app Firse try karle</h1>
+            <button className='hover:text-amber-600' onClick={() => reset()}>Fir se karle</button>
+        </div>
+    );
 }
+
